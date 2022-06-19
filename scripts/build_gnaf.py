21976b1267797464bc6905a33e73ecddb185bdad
Justin Evans <eustin.jvans@gmail.com>
2022-06-19 15:31:52 +1000

    create script to merge GNAF files

--- scripts/build_gnaf.py
+++ scripts/build_gnaf.py
@@ -2,28 +2,64 @@ import logging
 from glob import glob
 from pathlib import Path
 import re
-from time import time
-import numpy as np
 import pandas as pd
 
 STATES_TERRITORIES = ["ACT", "NSW", "NT", "QLD", "SA", "TAS", "VIC", "WA"]
+ADDRESS_DETAIL_TARGET_FIELDS = [
+    "BUILDING_NAME",
+    "LOT_NUMBER_PREFIX",
+    "LOT_NUMBER",
+    "LOT_NUMBER_SUFFIX",
+    "FLAT_TYPE_CODE",
+    "FLAT_NUMBER_PREFIX",
+    "FLAT_NUMBER",
+    "FLAT_NUMBER_SUFFIX",
+    "LEVEL_TYPE_CODE",
+    "LEVEL_NUMBER_PREFIX",
+    "LEVEL_NUMBER",
+    "LEVEL_NUMBER_SUFFIX",
+    "NUMBER_FIRST_PREFIX",
+    "NUMBER_FIRST",
+    "NUMBER_FIRST_SUFFIX",
+    "NUMBER_LAST_PREFIX",
+    "NUMBER_LAST",
+    "NUMBER_LAST_SUFFIX",
+    "STREET_LOCALITY_PID",
+    "LOCALITY_PID",
+    "POSTCODE",
+]
+STREET_LOCALITY_TARGET_FIELDS = [
+    "STREET_LOCALITY_PID",
+    "STREET_NAME",
+    "STREET_TYPE_CODE",
+    "STREET_SUFFIX_CODE",
+]
+LOCALITY_TARGET_FIELDS = ["LOCALITY_PID", "LOCALITY_NAME"]
 
 logging.basicConfig(level="INFO")
 logger = logging.getLogger(__name__)
 
 
-def main():
-    # validate dirs exist
-    def check_dirs_exist(parent_path, child_dir):
-        try:
-            dir_to_check = parent_path / child_dir
-            print(dir_to_check)
-            assert dir_to_check.exists()
-            return dir_to_check
-        except AssertionError:
-            logging.error(f"{dir_to_check.as_posix()} doesn't exist")
-            raise
+def create_file_map(fpath_list, sate_pattern):
+    states_list = [re.search(sate_pattern, x).group() for x in fpath_list]
+    return {k: v for k, v in zip(states_list, fpath_list)}
 
+
+def check_dirs_exist(parent_path, child_dir):
+    try:
+        dir_to_check = parent_path / child_dir
+        assert dir_to_check.exists()
+        return dir_to_check
+    except AssertionError:
+        logging.error(f"{dir_to_check.as_posix()} doesn't exist")
+        raise
+
+
+def df_from_pipe_delimited_file(fpath):
+    return pd.read_csv(fpath, delimiter="|")
+
+
+def main():
     try:
         gnaf_dir = Path(glob("data/G-NAF/G-NAF*").pop())
     except IndexError:
@@ -33,50 +69,63 @@ def main():
     gnaf_standard_dir = check_dirs_exist(gnaf_dir, "Standard")
     gnaf_auth_code_dir = check_dirs_exist(gnaf_dir, "Authority Code")
 
-    # build file lists
+    # build file dicts
     address_detail_files = glob(
         (gnaf_standard_dir / "*_ADDRESS_DETAIL_psv.psv").as_posix()
     )
+    address_detail_dict = create_file_map(
+        address_detail_files, "(?<=\/)[A-Z]+(?=_ADDRESS_DETAIL_psv.psv)"
+    )
+
     street_locality_files = glob(
         (gnaf_standard_dir / "*_STREET_LOCALITY_psv.psv").as_posix()
     )
+    street_locality_dict = create_file_map(
+        street_locality_files, "(?<=\/)[A-Z]+(?=_STREET_LOCALITY_psv.psv)"
+    )
+
     locality_files = glob((gnaf_standard_dir / "*LOCALITY_psv.psv").as_posix())
     locality_files = [x for x in locality_files if "STREET" not in x]
+    locality_dict = create_file_map(
+        locality_files, "(?<=\/)[A-Z]+(?=_LOCALITY_psv.psv)"
+    )
+
     level_type_file = glob(
         (gnaf_auth_code_dir / "Authority_Code_LEVEL_TYPE_AUT_psv.psv").as_posix()
-    )
+    ).pop()
+    level_type_df = pd.read_csv(level_type_file, delimiter="|")
+    level_type_df.drop(columns="DESCRIPTION", inplace=True)
 
-    # create hashmaps for smaller datasets to keep them in memory
-    logger.info("creating Authority Code LEVEL_TYPE hashmap")
-    level_type_df = pd.read_csv(level_type_file.pop(), delimiter="|")
-    level_type_dict = {row.CODE: row.NAME for i, row in level_type_df.iterrows()}
+    for state, details_fpath in address_detail_dict.items():
+        logging.info(f"processing {state}")
+        street_locality_fpath = street_locality_dict[state]
+        locality_fpath = locality_dict[state]
 
-    def build_hashmap(fpath_list, state_territory_pattern, primary_key):
-        hashmap = {}
-        for file in fpath_list:
-            logging.info(f"creating hashmap for {file}")
-            try:
-                state_or_territory = re.search(state_territory_pattern, file).group()
-            except AttributeError:
-                logging.error(
-                    f"Could not match a state or territory name from file {file}"
-                )
-                raise
+        logging.info(f"reading details file: {details_fpath}")
+        details_df = df_from_pipe_delimited_file(details_fpath)[
+            ADDRESS_DETAIL_TARGET_FIELDS
+        ]
 
-            this_df = pd.read_csv(file, delimiter="|")
-            this_df.set_index(primary_key, inplace=True)
-            hashmap[state_or_territory] = this_df.to_dict(orient="index")
-        return hashmap
+        logging.info(f"reading street locality file: {street_locality_fpath}")
+        street_locality_df = df_from_pipe_delimited_file(street_locality_fpath)[
+            STREET_LOCALITY_TARGET_FIELDS
+        ]
 
-    street_locality_dict = build_hashmap(
-        street_locality_files,
-        "(?<=\/)[A-Z]+(?=_STREET_LOCALITY_psv.psv)",
-        "STREET_LOCALITY_PID",
-    )
-    locality_dict = build_hashmap(
-        locality_files, "(?<=\/)[A-Z]+(?=_LOCALITY_psv.psv)", "LOCALITY_PID"
-    )
+        logging.info(f"reading locality file: {locality_fpath}")
+        locality_df = df_from_pipe_delimited_file(locality_fpath)[
+            LOCALITY_TARGET_FIELDS
+        ]
 
+        logging.info(f"merging files")
+        address_df = pd.merge(
+            details_df, street_locality_df, on="STREET_LOCALITY_PID", how="inner"
+        )
+        address_df = pd.merge(address_df, locality_df, on="LOCALITY_PID", how="inner")
+        assert address_df.shape[0] == details_df.shape[0]
+
+        out_path = f"data/address_file_{state}.csv"
+        logging.info(f"saving CSV file to {out_path}")
+        address_df.to_csv(out_path, index=False)
 
 if __name__ == "__main__":
     main()
