706095199fa41d34ba4f8429939328bc25268aa0
Justin Evans <eustin.jvans@gmail.com>
2022-06-21 19:02:10 +1000

    create script to load address data into elastic search

--- scripts/load_elasticsearch.py
+++ scripts/load_elasticsearch.py
@@ -1,14 +1,37 @@
 import os
 import dotenv
+from glob import glob
+import logging
+import csv
+from collections import deque
 from elasticsearch import Elasticsearch
+from elasticsearch.helpers import parallel_bulk
 
 dotenv.load_dotenv()
+logging.basicConfig(level="INFO")
+logger = logging.getLogger(__name__)
 
-es_client = Elasticsearch(
-    "https://localhost:9200",
-    ca_certs="ca.crt",
-    basic_auth=("elastic", os.environ["ELASTIC_PASSWORD"]),
-)
+def main():
+    es_client = Elasticsearch(
+        "https://localhost:9200",
+        ca_certs="ca.crt",
+        basic_auth=("elastic", os.environ["ELASTIC_PASSWORD"]),
+    )
 
-resp = es_client.info()
-print(resp)
+    address_files = glob("data/address_file_*.csv")
+
+    def address_generator(file_path):
+        csv_reader = csv.DictReader(open(file_path))
+        for row in csv_reader:
+            yield {
+                "_id": row["ADDRESS_DETAIL_PID"],
+                "_index": "addresses",
+                "body": f'{row["STREET_NAME"]} {row["STREET_TYPE_CODE"]}, {row["LOCALITY_NAME"]}' 
+            }
+
+    for file in address_files:
+        logger.info(f"loading file into elasticsearch: {file}")    
+        deque(parallel_bulk(es_client, address_generator(file)), maxlen=0)
+
+if __name__ == "__main__":
+    main()
