84323a2f582d4839ce79b596f69f8940fd3d9a4a
Justin Evans <eustin.jvans@gmail.com>
2022-06-27 16:46:17 +1000

    server: fix cors header

--- server/src/app.js
+++ server/src/app.js
@@ -4,7 +4,7 @@ const addressSearchRouter = require("./routes/addressSearch.router");
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "http://localhost3000" }));
+app.use(cors({ origin: "http://localhost:3000" }));
 
 app.use("/search", addressSearchRouter);
 
