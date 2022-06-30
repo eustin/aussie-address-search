9fd65eb22d718879bce91a0c34e961c50d5fc331
Justin Evans <eustin.jvans@gmail.com>
2022-06-30 15:20:53 +1000

    server: create suggestions

--- server/src/routes/addressSearch.controller.js
+++ server/src/routes/addressSearch.controller.js
@@ -2,6 +2,7 @@ const searchAddress = require("../models/addressSearch.model");
 
 async function httpSearchForAddress(req, res) {
   const address = req.body;
+
   if (!address.searchInput) {
     return res.status(400).json({
       error: "Missing searchInput property",
@@ -9,10 +10,10 @@ async function httpSearchForAddress(req, res) {
   }
 
   const results = await searchAddress(address.searchInput);
+  const hits = results.hits?.hits;
+  const suggestions = hits ? hits.map((hit) => hit._source.body) : [];
 
-  return res.status(200).json({
-    message: results,
-  });
+  return res.status(200).json({ suggestions });
 }
 
 module.exports = httpSearchForAddress;
