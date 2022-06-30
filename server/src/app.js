2d756b06a31eb235ce75c11a81d3314daa42a2ba
Justin Evans <eustin.jvans@gmail.com>
2022-07-01 07:09:15 +1000

    server: serve react app from server

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,16 @@
+const path = require("path");
 const express = require("express");
 const cors = require("cors");
 const addressSearchRouter = require("./routes/addressSearch.router");
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(express.static(path.join(__dirname, "..", "public")));
+app.use(cors({ origin: "http://localhost:8000" }));
 
 app.use("/search", addressSearchRouter);
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 module.exports = app;
